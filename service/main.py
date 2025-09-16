9109212d6e8d9eff548256a43b373fcdedb95312
Fabio Ayres <fabioja@insper.edu.br>
2025-09-16 14:56:11 -0300

    feat: finished microservice

--- service/main.py
+++ service/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+import joblib
+from pydantic import BaseModel
+
+
+class InputData(BaseModel):
+    MedInc: float
+    HouseAge: float
+    AveRooms: float
+    AveBedrms: float
+    Population: float
+    AveOccup: float
+    Latitude: float
+    Longitude: float
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_prediction():
-    return {"message": "This is a prediction service."}
+with open("../best_model.joblib", "rb") as f:
+    model = joblib.load(f)
+
+
+@app.post("/prediction")
+def read_prediction(item: InputData):
+    prediction = model.predict([list(item.model_dump().values())])[0]
+    return {
+        "message": "This is a prediction service.",
+        "data": item,
+        "prediction": prediction,
+    }
